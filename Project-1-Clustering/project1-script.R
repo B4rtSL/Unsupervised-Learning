d68435df7634fccdf89020aa293db4361ce5f33f
B4rtSL <bartsl1029@gmail.com>
2023-12-10 20:31:01 +0100

    feat: Markdown + basic cleaning, stats, correlation

--- Project-1-Clustering/project1-script.R
+++ Project-1-Clustering/project1-script.R
@@ -12,11 +12,44 @@ library(hopkins)
 library(NbClust)
 library(tidyverse)
 
+setwd('D:/Git-Management/Unsupervised-Learning/Project-1-Clustering')
+
+
 data <- read.csv('nba_2022-23_all_stats_with_salary.csv')
-head(data)
 str(data)
 
-stats <- data[c(7:52)]
-scaled_stats <- scale(stats)
+
+data$Position = as.factor(data$Position)
+summary(data$Position)
+
+# There are 7 players that are assigned with a mix of o classically considered basketball court positions. 
+# In order to achieve coherent position labeling, I have decided to manually assign positions (PG, SG, SF, PF, C) 
+# to those players taking into consideration either their historical roles or their role in lineups for the season.
+
+data[data$Position == 'PG-SG' | data$Position == 'SF-PF' | data$Position == 'SF-SG' | data$Position == 'SG-PG',]
+
+# 'TRB', 'PTS', 'AST', 'STL', 'BLK', 
+shorter_stats <- data[c('Player.Name', 'Position', 'MP', 'PTS', 'AST', 'STL', 'BLK', 'TRB', 'eFG.', 'TS.', 'USG.', 'WS.48','VORP')]
+length(shorter_stats[,1])
+shorter_stats[!complete.cases(shorter_stats),]
+
+shorter_stats = shorter_stats[complete.cases(shorter_stats),]
+length(shorter_stats[,1])
+
+stats = shorter_stats[, c('MP', 'PTS', 'AST', 'STL', 'BLK', 'TRB', 'eFG.', 'TS.', 'USG.', 'WS.48','VORP')]
+shorter_stats[437,]
+
+scaled_stats <- as.data.frame(lapply(stats, scale))
+shorter_stats[248,]
 get_clust_tendency(scaled_stats, 2, graph=T)
-hopkins(scaled_stats)
+?get_clust_tendency
+
+?kmeans
+
+fviz_nbclust(scaled_stats, FUNcluster = kmeans, method = 'silhouette') + theme_classic()
+?eclust
+stats_clust <- eclust(scaled_stats, FUNcluster = 'kmeans', k=2, graph=F)
+fviz_cluster(stats_clust, data = scaled_stats, elipse.type = 'convex')
+fviz_silhouette(stats_clust)
+
+data
