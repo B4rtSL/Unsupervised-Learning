2d22b47a3ebad55aaae2181abb59d907acedc0bb
B4rtSL <bartsl1029@gmail.com>
2024-01-04 15:19:26 +0100

    feat: KMO without NA's

--- Project-2-Dimension-Reduction/dataset-testing.R
+++ Project-2-Dimension-Reduction/dataset-testing.R
@@ -39,3 +39,31 @@ corrplot(cor_life, method = 'number', type = "lower", order = "hclust", tl.col =
 
 cortest.bartlett(cor_life)
 KMO(cor_life)
+
+### filling NA's with means
+
+life_num[!complete.cases(life_num),]
+columns_with_na <- colnames(life_num)[colSums(is.na(life_num)) > 0]
+
+summary(life_num)
+
+library(zoo)
+life_num_filled = na.aggregate(life_num, FUN = mean)
+life_num_filled[!complete.cases(life_num_filled),]
+
+life_num[is.na(life_num$Life.expectancy),"Life.expectancy"] <- mean(life_num$Life.expectancy, na.rm=T)
+
+life_num$Life.expectancy == life_num_filled$Life.expectancy
+
+### checking factorability after filling missing values
+
+life_num = life[, 4:22]
+life_num = life_num[complete.cases(life_num),]
+
+life_scaled = scale(life_num_filled)
+
+cor_life = cor(life_scaled)
+corrplot(cor_life, method = 'number', type = "lower", order = "hclust", tl.col = "black", tl.cex = 0.7, number.cex = 0.9)
+
+cortest.bartlett(cor_life)
+KMO(cor_life)
